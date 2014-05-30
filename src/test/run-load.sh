c5d7fb6db9489a41e5e919e97060b9cb17f788c8
Dean Pehrsson-Chapman <dean@p14n.com>
2014-05-30 06:21:26 +0100

    Updating versions for 1.0.0 release with vertx 2.1

--- src/test/run-load.sh
+++ src/test/run-load.sh
@@ -1,1 +1,1 @@
-java -cp ../../build/classes/test/:../../build/mods/p14n~vert-zeromq~0.0.1/lib/jeromq-0.3.1.jar com.p14n.zeromq.vertx.ZeroMQBridgeTest 50 2500 
+java -cp ../../build/classes/test/:../../build/mods/p14n~vert-zeromq~1.0.0/lib/jeromq-0.3.2.jar com.p14n.zeromq.vertx.ZeroMQBridgeTest 50 2500 
